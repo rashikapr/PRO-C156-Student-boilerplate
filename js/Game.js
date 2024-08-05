127dea01ba0024b1a5a026bb2625ff1005f82414
rashikapr <74289013+rashikapr@users.noreply.github.com>
2024-08-05 15:21:38 +0530

    startTimer

--- js/Game.js
+++ js/Game.js
@@ -14,31 +14,7 @@ AFRAME.registerComponent("game-play", {
   },
 
   startTimer: function (duration, timerEl) {
-    var minutes;
-    var seconds;
-
-    setInterval(()=> {
-      if (duration >=0) {
-        minutes = parseInt(duration / 60);
-        seconds = parseInt(duration % 60);
-
-        if (minutes < 10) {
-          minutes = "0" + minutes;
-        }
-        if (seconds < 10) {
-          seconds = "0" + seconds;
-        }
-
-        timerEl.setAttribute("text", {
-          value: minutes + ":" + seconds,
-        });
-
-        duration -= 1;
-      } 
-      else {
-        this.gameOver();        
-      }
-    },1000)
+     
   },
   isCollided: function (elemntId) {
     var element = document.querySelector(elemntId);
